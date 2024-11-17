3792681dfbea3f97a12b1fea7b48cbd6919be628
metastable-void <git@yuka.h09.eu>
2024-11-18 04:53:00 +0900

    implemented sample text speech

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
 # tohoku-tts-voicevox
 
+**ジェネリック東北共通語読み上げソフト** (VoiceVox Core 版)
+
 ジェネリックな東北共通語 (他所行きの発話) 風の音声合成ができるソフトです。
+
 いわゆる「標準語」を訛らせて発話させることを想定したもので，伝統的な方言 (津軽弁、南部弁、ケセン語、会津弁など) を再現することを目的としたものではありません。
+
 以下のバージョンを実装しています。
 
 - 北東北 (北奥羽アクセント圏)
 - 南東北 (無アクセント圏)
 
-小規模な簡易ネイティブチェックを行い，合成音声の範囲内で自然さには配慮しておりますが，精密に特定の場所の方言に準じてつくっているわけではないので，方言の参照用として使うことを想定したものではありませんので，ご注意ください。
+小規模な簡易ネイティブチェックを行い、合成音声の範囲内で自然さには配慮しておりますが、精密に特定の場所の方言に準じてつくっているわけではございません。
+
+また、方言の参照用として使うことを想定したものではありませんので，ご注意ください。
 
 このソフトウェアは [VoiceVox Core](https://github.com/VOICEVOX/voicevox_core) を使用しております。
 VoiceVox Core 側が配布している音源モデルは自由なライセンス (OSS的な意味で) ではありませんので，利用規約にご注意ください。
@@ -23,7 +29,7 @@ git clone https://github.com/metastable-void/tohoku-tts-voicevox.git
 cd tohoku-tts-voicevox
 ./download-deps.sh
 ./build.sh
-echo 'このように、日本語の任意の文章を方言風のアクセント・イントネーションで読みあげさせることができます。' | RUST_LOG=debug ./tohoku-tts-voicevox test-synthesis --variant northern --pitch-offset=-2 | ffplay -i -
+RUST_LOG=debug ./tohoku-tts-voicevox test-synthesis --variant northern --pitch-offset=-2 --speak-sample-text | ffplay -i -
 ```
 
 ## License
--- src/bin/tohoku-tts-voicevox.rs
+++ src/bin/tohoku-tts-voicevox.rs
@@ -6,6 +6,9 @@ use std::io::Read;
 
 use clap::{Parser, Subcommand};
 
+pub(crate) const VERSION: &str = env!("CARGO_PKG_VERSION");
+pub(crate) const PKG_NAME_JA: &str = "ジェネリック東北共通語読み上げソフト";
+
 #[derive(Debug, Parser)]
 #[command(about = "A simple CLI for Voicevox Core", long_about = None, version)]
 struct Cli {
@@ -33,6 +36,10 @@ enum Command {
         /// Speed scale
         #[arg(long, default_value_t = SynthesisParams::default().speed_scale())]
         speed_scale: f64,
+
+        /// Speak the program version and sample text (ignores input)
+        #[arg(long)]
+        speak_sample_text: bool,
     },
 }
 
@@ -42,30 +49,50 @@ fn main() -> anyhow::Result<()> {
     let args = Cli::parse();
 
     match args.subcommand {
-        Command::TestSynthesis { variant, pitch_offset, pitch_range, speed_scale } => {
+        Command::TestSynthesis { variant, pitch_offset, pitch_range, speed_scale, speak_sample_text } => {
             let params = SynthesisParams::new(pitch_offset, pitch_range, speed_scale)?;
             let options = SynthesisOptions {
                 params,
                 variant,
             };
-            test_synthesis(options)?;
+
+            if speak_sample_text {
+                let text = sample_text();
+                test_synthesis(options, &text)?;
+            } else {
+                let mut text = String::new();
+                let _ = std::io::stdin().read_to_string(&mut text)?;
+                test_synthesis(options, &text)?;
+            }
         },
     }
 
     Ok(())
 }
 
-fn test_synthesis(options: SynthesisOptions) -> anyhow::Result<()> {
+fn test_synthesis(options: SynthesisOptions, text: &str) -> anyhow::Result<()> {
     let dir = "./voicevox_core/open_jtalk_dic_utf_8-1.11";
     
     tohoku::initialize(dir)?;
 
     let handle = EngineHandle::new()?;
 
-    let mut text = String::new();
-    let _ = std::io::stdin().read_to_string(&mut text)?;
-    let wav = handle.synthesize_blocking(text, options)?;
+    let wav = handle.synthesize_blocking(text.to_owned(), options)?;
     std::io::stdout().write_all(wav.as_slice())?;
 
     Ok(())
+}
+
+fn sample_text() -> String {
+    format!(r#"
+これは、{}、バージョン{}です。
+これは、ジェネリックな東北共通語っぽい音声合成ができるソフトです。 
+このように、一般的な現代日本語の任意の文章を方言風のアクセント・イントネーションで読みあげさせることができます。
+いわゆる標準語を訛らせて発話させることを想定したもので、伝統的な方言、例えば津軽弁、南部弁、ケセン語、会津弁などを再現することを目的としたものではありません。
+小規模な簡易ネイティブチェックを行い、合成音声の範囲内で自然さには配慮しておりますが、精密に特定の場所の方言に準じてつくっているわけではありません。
+また、方言の参照用として使うことを想定したものではありません。
+ご注意ください。
+このソフトウェアは、アパッチライセンス・バージョン2.0のもとでライセンスされています。
+また、このソフトウェアは、ボイスボックス・コアを使用しております。
+"#, PKG_NAME_JA, VERSION.replace(".", "てん"))
 }
